81a34723302833f800b7bbf5623575a88e845324
Gabb <40926666+GabbNicoh@users.noreply.github.com>
2023-05-20 08:58:55 +0800

    tried a different approach

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:plmapp_authlogin/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MainApp());
