f0d43b543e475034d40a08d1f0730e15a63efb80
Gabb <40926666+gi-Gabb-byte@users.noreply.github.com>
2023-06-08 13:24:23 +0800

    Added landing page and updated Health declaration

--- lib/pages/healthdeclaration.dart
+++ lib/pages/healthdeclaration.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HealthDeclaration extends StatelessWidget {
+  const HealthDeclaration({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Welcome to Flutter',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('PLM Health Declaration'),
-            centerTitle: true,
-          ),
-          body: const WebView(
-            initialUrl: 'https://web4.plm.edu.ph/StudentHealthDeclaration/',
-            javascriptMode: JavascriptMode.unrestricted,
-          ),
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PLM Health Declaration'),
+        centerTitle: true,
+      ),
+      body: const WebView(
+        initialUrl: 'https://web4.plm.edu.ph/StudentHealthDeclaration/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import '../auth.dart';
+import '../pages/healthdeclaration.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -29,21 +30,121 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: _title(),
-      ),
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _userUid(),
-              _signOutButton(),
-            ],
-          )),
-    );
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: const Text('Pamantasan ng Lungsod ng Maynila'),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              child: const Text(
+                'Welcome!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'lib/images/plm.png',
+                  width: 275,
+                ),
+                Column(
+                  children: [
+                    // DATE,
+                    ElevatedButton(
+                      child: const Text('Events'),
+                      onPressed: () {
+                        debugPrint('Outlined Button');
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  child: const Text('ABOUT PLM'),
+                  onPressed: () {
+                    debugPrint('Outlined Button');
+                  },
+                ),
+                OutlinedButton(
+                  child: const Text('MAP'),
+                  onPressed: () {
+                    debugPrint('Outlined Button');
+                  },
+                ),
+                OutlinedButton(
+                  child: const Text('HEALTH DECLARATION'),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HealthDeclaration()));
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  child: const Text('ACADEMIC \nCALENDAR'),
+                  onPressed: () {
+                    debugPrint('Outlined Button');
+                  },
+                ),
+                OutlinedButton(
+                  child: const Text('ADMISSION'),
+                  onPressed: () {
+                    debugPrint('Outlined Button');
+                  },
+                ),
+                OutlinedButton(
+                  child: const Text('LIBRARY'),
+                  onPressed: () {
+                    debugPrint('Outlined Button');
+                  },
+                ),
+              ],
+            ),
+            _signOutButton(),
+          ],
+        )));
   }
+
+  // commented just in case need in the future
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: _title(),
+  //     ),
+  //     body: Container(
+  //       height: double.infinity,
+  //       width: double.infinity,
+  //       padding: const EdgeInsets.all(20),
+  //       child: Column(
+  //         crossAxisAlignment: CrossAxisAlignment.center,
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           _userUid(),
+  //           _signOutButton(),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
